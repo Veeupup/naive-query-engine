5c6f3d52722190b5852ebea63bef059f6dda1449
ywqzzy <592838129@qq.com>
2022-05-24 22:12:11 +0800

    exeucte cross join.

--- src/physical_plan/cross_join.rs
+++ src/physical_plan/cross_join.rs
@@ -8,6 +8,17 @@ use crate::logical_plan::plan::JoinType;
 use crate::logical_plan::schema::NaiveSchema;
 
 use crate::Result;
+use arrow::array::Array;
+use arrow::array::Float64Array;
+use arrow::array::Int64Array;
+use arrow::array::PrimitiveArray;
+use arrow::array::StringArray;
+use arrow::array::UInt64Array;
+use arrow::datatypes::DataType;
+use arrow::datatypes::Float64Type;
+use arrow::datatypes::Int64Type;
+use arrow::datatypes::SchemaRef;
+use arrow::datatypes::UInt64Type;
 use arrow::record_batch::RecordBatch;
 use std::sync::Arc;
 
@@ -44,35 +55,137 @@ impl PhysicalPlan for CrossJoin {
 
     fn execute(&self) -> Result<Vec<RecordBatch>> {
         // TODO(ywq)
-        let _outer_table = self.left.execute()?;
-        let _inner_table = self.right.execute()?;
+        let outer_table = self.left.execute()?;
+        let inner_table = self.right.execute()?;
 
-        let batches: Vec<RecordBatch> = vec![];
+        let mut batches: Vec<RecordBatch> = vec![];
 
-        // let left_rows = outer_table[0].num_rows();
-        // let right_rows = inner_table[0].num_rows();
-        // let mut columns = vec![];
-        // for outer in &outer_table {
-        //     let data_vec = vec![];
-        //     for i in 0..self.left.schema().fields().len() {
-        //         let array = outer.column(i);
-        //         for j in 0..right_rows {
-        //             // push to vector
-        //         }
-        //     }
-        // }
-
-        // for inner in &inner_table {
-        //     let data_vec = vec![];
-        //     for i in 0..self.right.schema().fields().len() {
-        //         let array = inner.column(i);
-        //         for j in 0..left_rows {
-        //             // push to vector
-        //         }
-        //     }
-        //     // data_vec to column
-        //     // push column into columns
-        // }
+        for outer in &outer_table {
+            for inner in &inner_table {
+                let mut columns: Vec<Arc<dyn Array>> = vec![];
+                let left_rows = outer.num_rows();
+                let right_rows = inner.num_rows();
+                println!("{} {}", left_rows, right_rows);
+                for i in 0..self.left.schema().fields().len() {
+                    let array = outer.column(i);
+                    let dt = self.left.schema().field(i).data_type();
+                    match dt {
+                        // TODO(ywq reafctor with macro)
+                        DataType::Int64 => {
+                            let mut t_vec = vec![];
+                            let left_col = array
+                                .as_any()
+                                .downcast_ref::<PrimitiveArray<Int64Type>>()
+                                .unwrap();
+                            for j in 0..right_rows {
+                                for k in 0..left_col.len() {
+                                    t_vec.push(left_col.value(k))
+                                }
+                            }
+                            columns.push(Arc::new(Int64Array::from(t_vec)));
+                        }
+                        DataType::UInt64 => {
+                            let mut t_vec = vec![];
+                            let left_col = array
+                                .as_any()
+                                .downcast_ref::<PrimitiveArray<UInt64Type>>()
+                                .unwrap();
+                            for j in 0..right_rows {
+                                for k in 0..left_col.len() {
+                                    t_vec.push(left_col.value(k))
+                                }
+                            }
+                            columns.push(Arc::new(UInt64Array::from(t_vec)));
+                        }
+                        DataType::Float64 => {
+                            let mut t_vec = vec![];
+                            let left_col = array.as_any()
+                                .downcast_ref::<PrimitiveArray<Float64Type>>()
+                                .unwrap();
+                            for j in 0..right_rows {
+                                for k in 0..left_col.len() {
+                                    t_vec.push(left_col.value(k))
+                                }
+                            }
+                            columns.push(Arc::new(Float64Array::from(t_vec)));
+                        }
+                        DataType::Utf8 => {
+                            let mut t_vec = vec![];
+                            let left_col = array.as_any()
+                                .downcast_ref::<StringArray>()
+                                .unwrap();
+                            for j in 0..right_rows {
+                                for k in 0..left_col.len() {
+                                    t_vec.push(left_col.value(k))
+                                }
+                            }
+                            columns.push(Arc::new(StringArray::from(t_vec)));
+                        }
+                        _ => unimplemented!(),
+                    }
+                }
+                for i in 0..self.right.schema().fields().len() {
+                    let array = inner.column(i);
+                    let dt = self.right.schema().field(i).data_type();
+                    match dt {
+                        DataType::Int64 => {
+                            let mut t_vec = vec![];
+                            let left_col = array
+                                .as_any()
+                                .downcast_ref::<PrimitiveArray<Int64Type>>()
+                                .unwrap();
+                            for j in 0..left_rows {
+                                for k in 0..left_col.len() {
+                                    t_vec.push(left_col.value(k))
+                                }
+                            }
+                            columns.push(Arc::new(Int64Array::from(t_vec)));
+                        }
+                        DataType::UInt64 => {
+                            let mut t_vec = vec![];
+                            let left_col = array
+                                .as_any()
+                                .downcast_ref::<PrimitiveArray<UInt64Type>>()
+                                .unwrap();
+                            for j in 0..left_rows {
+                                for k in 0..left_col.len() {
+                                    t_vec.push(left_col.value(k))
+                                }
+                            }
+                            columns.push(Arc::new(UInt64Array::from(t_vec)));
+                        }
+                        DataType::Float64 => {
+                            let mut t_vec = vec![];
+                            let left_col = array.as_any()
+                                .downcast_ref::<PrimitiveArray<Float64Type>>()
+                                .unwrap();
+                            for j in 0..left_rows {
+                                for k in 0..left_col.len() {
+                                    t_vec.push(left_col.value(k))
+                                }
+                            }
+                            columns.push(Arc::new(Float64Array::from(t_vec)));
+                        }
+                        DataType::Utf8 => {
+                            let mut t_vec = vec![];
+                            let left_col = array.as_any()
+                                .downcast_ref::<StringArray>()
+                                .unwrap();
+                            for j in 0..left_rows {
+                                for k in 0..left_col.len() {
+                                    t_vec.push(left_col.value(k))
+                                }
+                            }
+                            columns.push(Arc::new(StringArray::from(t_vec)));
+                        }
+                        _ => unimplemented!(),
+                    }
+                }
+                // new batch
+                let batch = RecordBatch::try_new(SchemaRef::from(self.schema.clone()), columns)?;
+                batches.push(batch);
+            }
+        }
         Ok(batches)
     }
 
